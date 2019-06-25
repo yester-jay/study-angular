6d198ec91bcb5709b0f080fb087137a22a72a27c
Jay <yester_jay@naver.com>
2019-06-26 00:22:23 +0900

    buttons component : data binding test

--- front-end/src/app/section/buttons/buttons.component.ts
+++ front-end/src/app/section/buttons/buttons.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
 
+  count = 0;
+
   constructor() { }
+
+  start(){
+    this.count ++;
+  }
 
   ngOnInit() {
   }
